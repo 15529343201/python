8c49fe953f7929a7be3344b68b98c95093ec5bc5
15529343201 <15529343201@139.com>
2017-03-25 15:23:02 +0800

    add py

--- 29.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# _*_ coding=utf-8 _*_
-#连接字符串
-
-delimiter = ','
-mylist = ['Brazil','Russia','India','China']
-print delimiter.join(mylist)
--- 9.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# _*_ coding=utf-8 _*_
-#列表转换为字典
-
-i = ['a','b']
-l = [1,2]
-print([i,l])
-
